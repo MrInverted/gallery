2cf62f0cb89f6ccee5e91a1a06af63f8b33c2995
MrInverted <akson.dyuk@gmail.com>
2023-09-04 14:22:24 +0300

    text

--- components/List.jsx
+++ components/List.jsx
@@ -5,12 +5,12 @@ export default function List() {
       <h4>Реализованный функционал</h4>
       <ul className="list-group">
         <li className="list-group-item">Добавление новой карточки</li>
-        <li className="list-group-item">Загрузка и сохрание фотографии через formidable</li>
+        <li className="list-group-item">Загрузка и сохрание фотографии через formidable (Не работает на vercel)</li>
         <li className="list-group-item">Фильтрация по категория</li>
         <li className="list-group-item">Поиск по заголовку</li>
         <li className="list-group-item">Пагинация с возможностью выбора кол-ва элементов на странице</li>
         <li className="list-group-item">Открытие карточки</li>
-        <li className="list-group-item">Регистрация и авторизация через jwt и с выводом ошибки в форме</li>
+        <li className="list-group-item">Регистрация и авторизация через jwt и с выводом ошибки в форме (Не работает на vercel)</li>
         <li className="list-group-item">Удаление карточки, если авторизован, и если логин пользователя создавшего совпадает с удаляющим</li>
       </ul>
     </div>
